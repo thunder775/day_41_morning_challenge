299ad76255e40512be1e5329053848d42a33df78
McLarenCollege <hello@mclarencollege.in>
2019-11-29 08:29:44 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+// Part 1
+// Tower of Hanoi - 3 Moves
+// Write a function which will check win in 3 moves, use the code you wrote yesterday
+
+// Part 2
+// Tower of Hanoi - N Moves
+// Write a function which will check win in N moves for a given state of Tower of Hanoi
+// The function will take an additional parameter 'n' (number of moves)
+
+
+// Stretch: Show all moves which lead to win, given a state.
+main() {
+}
