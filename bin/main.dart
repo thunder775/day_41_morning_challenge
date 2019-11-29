e9f5ddefed84187de4ce9b025ef399f34bce607e
thunder775 <rahulmasih61@gmail.com>
2019-11-29 09:57:24 +0530

    done(via print statement)

--- bin/main.dart
+++ bin/main.dart
@@ -7,7 +7,86 @@
 // Write a function which will check win in N moves for a given state of Tower of Hanoi
 // The function will take an additional parameter 'n' (number of moves)
 
-
 // Stretch: Show all moves which lead to win, given a state.
+
+List<List<List<int>>> possibleMoves(List<List<int>> input) {
+  List<List<List<int>>> output = [];
+  for (int i = 0; i < input.length; i++) {
+    for (int j = 0; j < input.length; j++) {
+      if (i != j) {
+        if (canBeStacked(input[i], input[j])) {
+          input[j].insert(0, input[i].removeAt(0));
+
+          addToOutputList(output, input);
+
+          input[i].insert(0, input[j].removeAt(0));
+        }
+      }
+    }
+  }
+  return output;
+}
+
+checkWinInNMoves(List<List<int>> input, int n) {
+//  List<List<List<int>>> output =[];
+  List<List<List<int>>> allMoves = possibleMoves(input);
+  if (n == 1) {
+    return checkWinInOneMove(input);
+  }
+  for (var move in allMoves) {
+    if (checkWinInNMoves(move, n - 1) != null) {
+      print(move);
+//    output.addAll(checkWinInNMoves(input, n-1));
+      return move;
+    }
+  }
+//  return output;
+return null;
+}
+
+List<List<int>> checkWinInOneMove(List<List<int>> input) {
+
+  List<List<List<int>>> allMoves = possibleMoves(input);
+  for (var move in allMoves) {
+    if (isFinalMove(move)) {
+      print(move);
+      return move;
+    }
+  }
+  return null;
+}
+
+bool isFinalMove(List<List<int>> input) => input[0].isEmpty && input[1].isEmpty;
+
+void addToOutputList(List<List<List<int>>> output, List<List<int>> source) {
+  List<List<int>> temp = [];
+  for (List outer in source) {
+    List<int> innerList = [];
+    for (int i = 0; i < outer.length; i++) {
+      innerList.add(outer[i]);
+    }
+    temp.add(innerList);
+  }
+  output.add(temp);
+}
+
+bool canBeStacked(List<int> tower1, List<int> tower2) =>
+    (tower1.isNotEmpty && (tower2.isEmpty || tower1[0] < tower2[0]));
+
 main() {
+//  print(checkWinInNMoves([
+//    [1, 2],
+//    [],
+//    [3, 4]
+//  ], 3));
+//  print(checkWinInNMoves([
+//    [],
+//    [1, 2],
+//    [3, 4]
+//  ], 3));
+  print(checkWinInNMoves([
+    [1,2,3,4],
+    [],
+    []
+  ], 15));
 }
--- test/Day_41_Morning_Challenge_test.dart
+++ test/Day_41_Morning_Challenge_test.dart
@@ -1,6 +1,41 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
-  test('Test Case 1', () {
+  test('Test Case 1', () {});
+  test('hanoi', () {
+    expect(
+        checkWinInNMoves([
+          [1, 2],
+          [],
+          [3, 4]
+        ], 3),
+        [
+          [2],
+          [1],
+          [3, 4]
+        ]);
+    expect(
+        checkWinInNMoves([
+          [],
+          [1, 2],
+          [3, 4]
+        ], 3),
+        [
+          [1],
+          [2],
+          [3, 4]
+        ]);
+    expect(
+        checkWinInNMoves([
+          [1, 2, 3, 4],
+          [],
+          []
+        ], 15),
+        [
+          [2, 3, 4],
+          [1],
+          []
+        ]);
   });
 }
